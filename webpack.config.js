bcd1efd2afafc5bb44eecdb9e97ec538c13cf078
zhangxiaowei <zhangxiaowei@innobuddy.com>
2018-08-28 00:03:44 +0800

    chore: change build name

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,11 @@ module.exports = {
   mode: 'production',
   entry: {
     'simple-react-router': './src/index.js',
-    'path-to-regexp': './src/path-to-regexp.js',
-    'router-core': './src/router-core.js',
   },
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].js',
-    library: '[name]',
+    library: 'SimpleReactRouter',
     libraryTarget: 'umd',
   },
   module: {
